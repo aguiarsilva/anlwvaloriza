f4bcd1e41dba02e6b81611db71793af28abe27b0
aguiarsilva <aguiarsilva@gmail.com>
2021-06-22 10:50:33 -0300

    connect database

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,8 @@
+import "reflect-metadata";
 import express from "express";
 
-//@types/express
+import "./database";
+
 const app = express();
-
-/**
-* GET - Buscar uma informacao
-* POST - Inserir (Criar) uma informacao
-* PUT - alterar uma informacao
-* DELETE - remover uma informacao
-* PATCH - alterar uma informacao especifica
- */
-
-app.get('/test', (request, response, next) => {
-    //request - tudo que esta entrando
-    //response - tudo que esta saindo
-    return response.send("Ola NLW");
-});
-
-app.post('/test-post', (request, response, next) => {
-    return response.send("Ola metdodo POST NLW");
-});
 
 app.listen(3000, () => console.log("Server is Running!"));
