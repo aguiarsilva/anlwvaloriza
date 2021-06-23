ecec5213ce060e29f043896a0b6e8c67d4678d36
aguiarsilva <aguiarsilva@gmail.com>
2021-06-23 05:48:43 -0300

    added files

---
+++ README.md
@@ -0,0 +1,17 @@
+# NLW Valoriza
+
+## Regras
+
+- User Registration
+    [ ] It is not allowed to register more than one user with the same e-mail
+    [ ] It is not allowed to register a user without an e-mail
+
+- TAG Registration
+    [ ] It is not allowed to register more than a TAG with the same name
+    [ ] It is not allowed to register a TAG without a name
+    [ ] It is not allowed the registration by users that aren't administrators
+
+- Comments Registration
+    [ ] It is not allowed for an user to register a self compliment
+    [ ] It is not allowed to register a compliment for invalid users
+    [ ] The user must be authenticated in the app
---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,39 @@
+import { getCustomRepository } from "typeorm";
+import { UsersRepositories } from "../repositories/UsersRepositories";
+
+interface IUserRequest {
+    name: string;
+    email: string;
+    admin?: boolean;
+}
+
+class CreateUserService {
+
+    async execute({ name, email, admin } : IUserRequest) {
+        const usersRepository = getCustomRepository(UsersRepositories);
+//verify if there is a valid email
+        if(!email) {
+            throw new Error("email incorrect");
+        }
+
+        const userAlreadyExists = await usersRepository.findOne({
+            email,
+        });
+// verify if email already exists 
+        if(userAlreadyExists){
+            throw new Error("User already exists");
+        }
+// create instance
+        const user = usersRepository.create({
+            name,
+            email,
+            admin
+        })
+// save object to database
+        await usersRepository.save(user);
+
+        return user;
+    }
+}
+
+export { CreateUserService };
