b156a114bc57c837b71bcaaa6daac65e91e79578
aguiarsilva <aguiarsilva@gmail.com>
2021-06-26 06:45:10 -0300

    add compliment lists files

---
+++ src/services/ListUserReceivedComplimentService.ts
@@ -0,0 +1,22 @@
+import { getCustomRepository } from 'typeorm';
+import { ComplimentsRepositories } from '../repositories/ComplimentsRepositories';
+
+
+//Create list class
+class ListUserReceivedComplimentsService {
+    async execute(user_id: string) {
+        // get the compliments repository
+        const complimentsRepositories = getCustomRepository(ComplimentsRepositories);
+    
+// extract all the compliments from repository
+        const compliments = await complimentsRepositories.find({
+            where: {
+                user_receiver: user_id
+            }
+        })
+
+        return compliments
+    }
+}
+
+export { ListUserReceivedComplimentsService };
